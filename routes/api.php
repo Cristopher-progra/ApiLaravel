3cac0a39dae614a9d33102ff51f21362e30a8b70
Cristopher <cristophervalle696@gmail.com>
2023-11-04 23:01:04 -0600

    Cracion de ruta de login y controlador de usuario

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EstudianteController;
-
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,12 +15,13 @@ use App\Http\Controllers\EstudianteController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/registrar', [AuthController::class, 'store']);
 Route::delete('/estudiante/delete/{id}', [EstudianteController::class, 'deleteStudent']);
 Route::get('/estudiante/find/{id}', [EstudianteController::class, 'findStudent']);
 Route::put('/estudiante/update/{id}', [EstudianteController::class, 'updateStudent']);
 Route::post('/estudiante/store', [EstudianteController::class, 'storeStudent']);
 Route::get('/estudiante/select', [EstudianteController::class, 'selectStudent']);
-
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
